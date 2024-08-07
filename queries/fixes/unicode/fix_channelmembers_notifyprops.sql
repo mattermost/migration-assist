3889d941d0b7031882e8589a9d4aba0f53e17d41
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2024-08-07 17:38:10 +0200

    Simplify unicode fixes by removing the check for the column

--- queries/fixes/unicode/fix_channelmembers_notifyprops.sql
+++ queries/fixes/unicode/fix_channelmembers_notifyprops.sql
@@ -1,14 +1,1 @@
-SET @preparedStatement = (SELECT IF(
- (
-     SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
-     WHERE table_name = 'ChannelMembers'
-     AND table_schema = DATABASE()
-     AND column_name = 'NotifyProps'
- ) > 0,
- 'UPDATE ChannelMembers SET NotifyProps = REPLACE(NotifyProps, \'\\\\u0000\', \'\') WHERE NotifyProps LIKE \'%\\u0000%\';',
- 'SELECT 1'
-));
-
-PREPARE updateIfExists FROM @preparedStatement;
-EXECUTE updateIfExists;
-DEALLOCATE PREPARE updateIfExists;
+UPDATE ChannelMembers SET NotifyProps = REPLACE(NotifyProps, '\u0000', '') WHERE NotifyProps LIKE '%\u0000%';
--- queries/fixes/unicode/fix_users_notifyprops.sql
+++ queries/fixes/unicode/fix_users_notifyprops.sql
@@ -1,14 +1,1 @@
-SET @preparedStatement = (SELECT IF(
- (
-     SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
-     WHERE table_name = 'Users'
-     AND table_schema = DATABASE()
-     AND column_name = 'NotifyProps'
- ) > 0,
- 'UPDATE Users SET NotifyProps = REPLACE(NotifyProps, \'\\\\u0000\', \'\') WHERE NotifyProps LIKE \'%\\u0000%\';',
- 'SELECT 1'
-));
-
-PREPARE updateIfExists FROM @preparedStatement;
-EXECUTE updateIfExists;
-DEALLOCATE PREPARE updateIfExists;
+UPDATE Users SET NotifyProps = REPLACE(NotifyProps, '\u0000', '') WHERE NotifyProps LIKE '%\u0000%';
